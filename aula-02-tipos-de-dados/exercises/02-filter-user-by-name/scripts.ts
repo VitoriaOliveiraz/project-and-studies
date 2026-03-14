9e42239f7ed44b5776726e7cc399743698524b0f
Vhickyzambi <vitoriasilva55032@gmail.com>
2026-03-14 09:44:51 -0300

    feat: inserindo mais um exercicio e anotações

---
+++ aula-02-tipos-de-dados/exercises/02-filter-user-by-name/scripts.ts
@@ -0,0 +1,41 @@
+const usuarios: { nome: string; idade: number; status: boolean }[] = [
+  {
+    nome: "Guido",
+    idade: 32,
+    status: true,
+  },
+  {
+    nome: "Dani",
+    idade: 30,
+    status: true,
+  },
+  {
+    nome: "João",
+    idade: 40,
+    status: false,
+  },
+  {
+    nome: "Guilherme",
+    idade: 29,
+    status: true,
+  },
+  {
+    nome: "Ana",
+    idade: 18,
+    status: false,
+  },
+  {
+    nome: "José",
+    idade: 28,
+    status: false,
+  },
+];
+
+const encontrarPorNome = (
+  listaUsuarios: { nome: string; idade: number; status: boolean }[], texto: string
+) => {
+    const TemNoNome = listaUsuarios.filter(usuario => usuario.nome.includes(texto))
+    return TemNoNome
+};
+
+console.log(encontrarPorNome(usuarios, "Jo"))
